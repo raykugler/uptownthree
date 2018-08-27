c91a716843c2625b3273680eaf8103d76d9ed2d6
Ray Kugler <raymond.kugler@gmail.com>
2018-08-27 00:11:42 -0700

    super pre heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports =(env)=>{
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
-
     return{
 
         entry: './src/app.js',
@@ -19,26 +19,26 @@ module.exports =(env)=>{
             },{
                 test:/\.s?css$/,
                 use: CSSExtract.extract({
-                    use: [
+                    use.[
                         {
-                            loader: 'css-loader',
-                            options:{
-                               sourceMap: true
-                            }},
-                            {
-                            loader: 'sass-loader',
-                            options:{
-                                sourceMap: true
-                            }
-                            }
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap:true
+                    }},
+                    {
+                        loader: 'sass-loader',
+                        options:{
+                            sourceMap: true
+                        }
+                    }
                     ]
                 })
             }]
         },
-        plugins: [
+        plugin: [
             CSSExtract
         ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname , 'public'),
             historyApiFallback: true
